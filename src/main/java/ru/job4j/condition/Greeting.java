796c216952ddba506c7934c2aabb02e9a2b518bc
Igor <palygaiv@gmail.com>
2021-12-05 13:37:22 +0300

    1

--- src/main/java/ru/job4j/condition/Greeting.java
+++ src/main/java/ru/job4j/condition/Greeting.java
@@ -2,11 +2,12 @@ package ru.job4j.condition;
 
 public class Greeting {
     public static void main(String[] args) {
-        float f = 12358.7f;
-        int i = 45981;
-        short rsl = (short) (f + i);
-        System.out.println(rsl);
+        String idea = "I like java!";
+        System.out.println(idea);
+        String lyrics = "But I am a newbie.";
+        int year = 2021;
+        String separate = " ";
+        idea = idea + separate + lyrics + separate + year;
+        System.out.println(idea);
     }
 }
-
-
