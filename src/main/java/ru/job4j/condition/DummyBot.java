20d086aa3f563b49cb37080cdf6e5baf85bc9e9b
Igor <palygaiv@gmail.com>
2021-12-12 12:58:59 +0300

    4.1.1. Операторы сравнения в String. [#237406 #238634]

--- src/main/java/ru/job4j/condition/DummyBot.java
+++ src/main/java/ru/job4j/condition/DummyBot.java
@@ -4,8 +4,8 @@ public class DummyBot {
     public static String answer(String question) {
         String rsl = "Это ставит меня в тупик. Задайте другой вопрос.";
         if ("Привет, Бот.".equals(question)) {
-        rsl = "Привет, умник.";
-    } else if ("Пока.".equals(question)) {
+            rsl = "Привет, умник.";
+        } else if ("Пока.".equals(question)) {
             rsl = "До скорой встречи.";
         }
         return rsl;
---
+++ src/main/java/ru/job4j/condition/Max.java
@@ -0,0 +1,12 @@
+package ru.job4j.condition;
+
+public class Max {
+    public static int max(int left, int right) {
+        int result = left;
+        return result;
+
+        public static int summation(int first, int second) {
+            int result = first + second;
+            return result;
+    }
+}
