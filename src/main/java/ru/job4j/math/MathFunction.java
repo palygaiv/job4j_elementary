2b4b3dc7741bec14b3eff1c200c74f0bdb351861
Igor <palygaiv@gmail.com>
2022-01-05 11:57:19 +0300

    5.9.1. Генерация конструкций в IDEA [#279134]

--- src/main/java/ru/job4j/math/MathFunction.java
+++ src/main/java/ru/job4j/math/MathFunction.java
@@ -2,18 +2,22 @@ package ru.job4j.math;
 
 public class MathFunction {
     public static double sum(double first, double second) {
+
         return first + second;
     }
 
     public static double multiply(double first, double second) {
+
         return first * second;
     }
 
     public static double divide(double first, double second) {
+
         return first / second;
     }
 
     public static double sub(double first, double second) {
+
         return first - second;
      }
 }
