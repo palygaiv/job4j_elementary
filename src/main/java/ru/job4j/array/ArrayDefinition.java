f1e2f1054df8e7fb12bd9d13f6914d01c4c8bf37
Igor <palygaiv@gmail.com>
2022-01-12 14:47:15 +0300

    6.0.2. Заполнение массива. [#226300]

--- src/main/java/ru/job4j/array/ArrayDefinition.java
+++ src/main/java/ru/job4j/array/ArrayDefinition.java
@@ -8,5 +8,15 @@ public class ArrayDefinition {
         System.out.println("Размер массива равен: " + ages.length);
         System.out.println("Размер массива равен: " + surnames.length);
         System.out.println("Размер массива равен: " + prices.length);
+        System.out.println();
+        String[] names = new String[4];
+        names[0] = "Petr Arsentev";
+        names[1] = "Igor Palyga";
+        names[2] = "Ivan Ivanov";
+        names[3] = "Petr Petrov";
+        System.out.println(names[0]);
+        System.out.println(names[1]);
+        System.out.println(names[2]);
+        System.out.println(names[3]);
     }
 }
